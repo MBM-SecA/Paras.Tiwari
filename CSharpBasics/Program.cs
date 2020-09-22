533f89cf745e1b2e66bf2e2b24ed4b0c4bd74d87
Paras Tiwari <parastiware@gmail.com>
2020-09-22 11:09:36 +0545

    Changes in inheritance file

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -4,9 +4,9 @@ using System;
     {        static void Main()
         {
           Square square = new Square(34.5);
-         square.Display();
+          square.Display();
           Rectangle rectangle = new Rectangle(45.4,23.5);
-         rectangle.Display();
+          rectangle.Display();
           Circle circle = new Circle(45.4);
           circle.Display();
      
