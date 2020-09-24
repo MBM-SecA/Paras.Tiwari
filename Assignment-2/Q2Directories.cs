257989602305ae50ea77cf2ebd930365f9613f5d
Paras Tiwari <parastiware@gmail.com>
2020-09-24 07:57:29 +0545

    Assignment 2 completed

--- Assignment-2/Q2Directories.cs
+++ Assignment-2/Q2Directories.cs
@@ -1,11 +1,14 @@
 using System.IO;
 using System;
-public class Directories{
+using System.Text;
+
+public class Directories
+{
     public void CreateDirectories()
     {
-        string directoryName = "Parent";
+        string directoryName = @"D:\FIles0f6thsem\Example\Parent";
         string subDirectoryName="Subfolder";
-        string helloworldprogram = @"using System;
+        string helloworldprogram = @"using System; 
 
                                     namespace Assignment_2
                                     {
@@ -16,34 +19,34 @@ public class Directories{
                                                 Console.WriteLine( ""Hello World!"");
                                             }
                                         }
-                                    }  ";
+                                    }  "; //storing hellow world program data as string
+        byte[] bytes =  Encoding.UTF8.GetBytes(helloworldprogram);// converting to bytes 
+                                                                //to perform stream write
         
         Directory.CreateDirectory(directoryName); //creating parent directory
         for(int i=1;i<=10;i++) //creating 10 sub directories
         {
             string tempsubDirectoryName=$"{directoryName}/{subDirectoryName+i}";
             Directory.CreateDirectory(tempsubDirectoryName);
-        }
-          
-          for(int i=1;i<=10;i++)// creating helloworld program inside subdirectories
-        {
             string fileName = $"{directoryName}/{subDirectoryName+i}/Helloworld.cs";
-            File.Create(fileName);
-        }
-
-        for(int i=1;i<=10;i++)// Writing code in each program files
-        {
-            string fileName = $"{directoryName}/{subDirectoryName+i}/Helloworld.cs";
-            if(File.Exists(fileName))
-            {
-                File.WriteAllText(fileName,helloworldprogram);
-            }
+            // creating helloworld program inside subdirectories
+            // Writing code in each program files
+            using (var stream = File.Open(fileName, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+            { //using stream writer method to write the file
+               try{ 
+                    
+                        stream.Write(bytes,0,bytes.Length);
+                    } 
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
                 
-            else
-                Console.WriteLine("File not found to write content");
+               
+               
+            }
+         
         }
-        
-
         Console.WriteLine("Created folders and files sucessfully");
     }
 }
