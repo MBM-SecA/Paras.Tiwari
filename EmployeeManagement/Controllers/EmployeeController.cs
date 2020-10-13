d4d662443bf9181f79e6e1dbb09215a21f8d3422
Paras Tiwari <parastiware@gmail.com>
2020-10-13 11:49:59 +0545

    Added some changes

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,19 +1,23 @@
-using System.Collections.Generic;
+
+
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+
 public class EmployeeController: Controller
 {
+    
     public ActionResult Index()
     {
-   
-        //object initializer syntax
-        Person emp1 = new Person(){ FirstName = "Bishnu",LastName ="Rawal",Address ="Kathmandu",Salary =2000.0 };
-        Person emp2 = new Person(){ FirstName = "Bishnu",LastName ="Rawal",Address ="Kathmandu",Salary =2000.0 };
-        Person emp3 = new Person(){ FirstName = "Bishnu",LastName ="Rawal",Address ="Kathmandu",Salary =2000.0 };
-        Person emp4 = new Person(){ FirstName = "Bishnu",LastName ="Rawal",Address ="Kathmandu",Salary =2000.0 };
-        Person emp5 = new Person(){ FirstName = "Bishnu",LastName ="Rawal",Address ="Kathmandu",Salary =2000.0 };
-        Person emp6 = new Person(){ FirstName = "Bishnu",LastName ="Rawal",Address ="Kathmandu",Salary =2000.0 };
-        List<Person> employees = new List<Person>(){emp1,emp2,emp3,emp4,emp5,emp6};
+        var employees = Person.GetData();
+        
         return View(employees);
+    }
+    public ActionResult Detail(string firstName)
+    {  
+       var  employees = Person.GetData();
+        var employee= employees.FirstOrDefault(x=>x.FirstName==firstName);
+        return View(employee);
+        
     }
 
 }
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 public class Person
 {
     public string FirstName { get; set; }
@@ -6,6 +7,17 @@ public class Person
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
 
+    public static List<Person> GetData()
+    {//object initializer syntax
+        Person emp1 = new Person() { FirstName = "Bishnu", LastName = "Rawal", Address = "Kathmandu", Salary = 20000.0 };
+        Person emp2 = new Person() { FirstName = "Paras", LastName = "Tiwari", Address = "Pokhara", Salary = 2000.0 };
+        Person emp3 = new Person() { FirstName = "Cosmic", LastName = "Koirala", Address = "Butwal", Salary = 6000.0 };
+        Person emp4 = new Person() { FirstName = "Hari", LastName = "Sapkota", Address = "Pokhara", Salary = 2000.0 };
+        Person emp5 = new Person() { FirstName = "Sandesh", LastName = "Magar", Address = "Kathmandu", Salary = 5000.0 };
+        Person emp6 = new Person() { FirstName = "Kamal", LastName = "pandit", Address = "Kathmandu", Salary = 3000.0 };
+        List<Person> employees = new List<Person>() { emp1, emp2, emp3, emp4, emp5, emp6 };
 
+        return employees;
+    }
 
 }
