1ac97f333d583ad50d3338204a6ac414a1d748ef
Paras Tiwari <parastiware@gmail.com>
2020-11-26 09:03:23 +0545

    Updated content on EMployeeController

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,23 +1,34 @@
 
-
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController: Controller
 {
-    
+   
+     
     public ActionResult Index()
     {
-        var employees = Person.GetData();
         
+        var employees = Person.GetPeople();
         return View(employees);
     }
-    public ActionResult Detail(string firstName)
+    public ActionResult Detail([FromQuery]int id)
     {  
-       var  employees = Person.GetData();
-        var employee= employees.FirstOrDefault(x=>x.FirstName==firstName);
+       var  employees = Person.GetPeople();
+        Person employee= employees.FirstOrDefault(x=>x.Id==id);
         return View(employee);
         
+    }
+
+
+    public ActionResult Add()
+    {
+        return View();
+    }
+[HttpPost]
+    public ActionResult<string> Add([FromForm]Person person)
+    {
+        return "Employee Added successfully!!!";
     }
 
 }
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,23 +1,31 @@
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 public class Person
 {
+    public int Id { get; set; }
+    [Required]
+    [Display(Name = "First Name")]
     public string FirstName { get; set; }
+    [Required]
+    [Display(Name = "First Name")]
     public string LastName { get; set; }
     public string Address { get; set; }
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
+    public static List<Person> GetPeople()
+    {
+        Person p1 = new Person() { Id = 1, FirstName = "Bishnu", LastName = "Rawal", Address = "Kathmandu", Salary = 20000.0 };
+        Person p2 = new Person() { Id = 2, FirstName = "Paras", LastName = "Tiwari", Address = "Pokhara", Salary = 2000.0 };
+        Person p3 = new Person() { Id = 3, FirstName = "Cosmic", LastName = "Koirala", Address = "Butwal", Salary = 6000.0 };
+        Person p4 = new Person() { Id = 4, FirstName = "Hari", LastName = "Sapkota", Address = "Pokhara", Salary = 2000.0 };
+        Person p5 = new Person() { Id = 5, FirstName = "Sandesh", LastName = "Magar", Address = "Kathmandu", Salary = 5000.0 };
+        Person p6 = new Person() { Id = 6, FirstName = "Kamal", LastName = "pandit", Address = "Kathmandu", Salary = 3000.0 };
 
-    public static List<Person> GetData()
-    {//object initializer syntax
-        Person emp1 = new Person() { FirstName = "Bishnu", LastName = "Rawal", Address = "Kathmandu", Salary = 20000.0 };
-        Person emp2 = new Person() { FirstName = "Paras", LastName = "Tiwari", Address = "Pokhara", Salary = 2000.0 };
-        Person emp3 = new Person() { FirstName = "Cosmic", LastName = "Koirala", Address = "Butwal", Salary = 6000.0 };
-        Person emp4 = new Person() { FirstName = "Hari", LastName = "Sapkota", Address = "Pokhara", Salary = 2000.0 };
-        Person emp5 = new Person() { FirstName = "Sandesh", LastName = "Magar", Address = "Kathmandu", Salary = 5000.0 };
-        Person emp6 = new Person() { FirstName = "Kamal", LastName = "pandit", Address = "Kathmandu", Salary = 3000.0 };
-        List<Person> employees = new List<Person>() { emp1, emp2, emp3, emp4, emp5, emp6 };
+        List<Person> persons = new List<Person>() { p1, p2, p3, p4, p5, p6 };
+        return persons;
 
-        return employees;
     }
 
 }
